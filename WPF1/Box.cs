d1b5b174fc403790d836530e694d36872568edd6
student <student@K21-07-STUDENT>
2023-02-07 12:24:19 +0500

    Че то сделал

--- WPF1/Box.cs
+++ WPF1/Box.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,11 +14,16 @@ namespace WPF1
 {
     public class Box
     {
+        public delegate void Delegate(Box box);
+        Delegate OtDel;
+
         private Canvas CAN;
 
         SolidColorBrush BaseColor = new SolidColorBrush(Colors.Gray);
         SolidColorBrush FocusColor = new SolidColorBrush(Colors.LimeGreen);
         private Point MouesLastPosition;
+
+        private Point CirclePointLast;
 
         private Border BorderBorder;
         private Canvas canvas1;
@@ -27,16 +33,24 @@ namespace WPF1
         private const int CircleSize = 20;
         private const double defoultSize = 100;
         private bool Solution = false;
+        private bool SolutionForMouse = false;
         public Box(Canvas canvas, Shape rectangle)
         {
             BorderBorder = new Border();
             canvas1 = new Canvas();
-            canvas1.Background = Brushes.White;
             UpCircle = new Ellipse();
+            UpCircle.MouseLeftButtonDown += UpCircle_MouseLeftButtonDown;
+            UpCircle.MouseMove += UpCircle_MouseMove;
+            UpCircle.MouseUp += UpCircle_MouseUp;
             DownCircle = new Ellipse();
+            DownCircle.MouseLeftButtonDown += DownCircle_MouseLeftButtonDown;
+            DownCircle.MouseMove += DownCircle_MouseMove;
+            DownCircle.MouseUp += DownCircle_MouseUp;
             UpCircle.Width = DownCircle.Width = CircleSize;
             UpCircle.Height = DownCircle.Height = CircleSize;
             UpCircle.Fill = DownCircle.Fill = Brushes.Black;
+            BorderBorder.BorderThickness = new Thickness(3);
+            
             double PosCircle = CircleSize * 0.5 * -1;
             Canvas.SetTop(UpCircle, PosCircle);
             Canvas.SetLeft(UpCircle, PosCircle);
@@ -45,26 +59,94 @@ namespace WPF1
             Canvas.SetBottom(DownCircle, PosCircle);
             Canvas.SetRight(DownCircle, PosCircle);
             CAN = canvas;
-            rectangle.Width = defoultSize -2;
-            rectangle.Height = defoultSize - 2;
+            rectangle.Width = defoultSize - 6;
+            rectangle.Height = defoultSize - 6;
             rectangle.Fill = BaseColor;
             double left = (canvas.ActualWidth - rectangle.Width) / 2;
 
             double top = (canvas.ActualHeight - rectangle.Height) / 2;
-            
+
             Canvas.SetLeft(BorderBorder, left);
             Canvas.SetTop(BorderBorder, top);
             SHAP = rectangle;
             CreateRectangle(canvas);
 
 
-
+            
 
         }
+        public void Link(Delegate oDelegate)
+        {
+            OtDel = oDelegate;
+        }
+        private void DownCircle_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            SolutionForMouse = false;
+            Mouse.Capture(null);
+
+        }
+
+        private void DownCircle_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (SolutionForMouse == false) { return; }
+            Point CirclePointFirst = e.GetPosition(CAN);
+
+            double Right = BorderBorder.Width + CirclePointFirst.X - CirclePointLast.X;
+            double Height = BorderBorder.Height + CirclePointFirst.Y - CirclePointLast.Y;
+            if (Right <= 10 || Height <= 10) { return; }
+            BorderBorder.Width = Right;
+            BorderBorder.Height = Height;
+
+            SHAP.Width = Right - 6;
+            SHAP.Height = Height - 6;
+            CirclePointLast = CirclePointFirst;
+
+        }
+
+        private void DownCircle_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            SolutionForMouse = true;
+            CirclePointLast = e.GetPosition(CAN);
+            canvas1.Background = Brushes.Gray;
+            Mouse.Capture(DownCircle);
+        }
+
+        private void UpCircle_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            SolutionForMouse = false;
+            Mouse.Capture(null);
+        }
+
+        private void UpCircle_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (SolutionForMouse == false) { return; }
+
+            Point CirclePoinFirst = e.GetPosition(CAN);
+
+            double Left = BorderBorder.Width + CirclePointLast.X - CirclePoinFirst.X;
+            double Top = BorderBorder.Height + CirclePointLast.Y - CirclePoinFirst.Y;
+            Canvas.SetLeft(BorderBorder, CirclePoinFirst.X);
+            Canvas.SetTop(BorderBorder, CirclePoinFirst.Y);
+            if (Left <= 2 || Top <= 2) { return; }
+            BorderBorder.Width = Left;
+            SHAP.Width = Left - 6;
+            BorderBorder.Height = Top;
+            SHAP.Height = Top - 6;
+            CirclePointLast = CirclePoinFirst;
+
+        }
+
+        private void UpCircle_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            SolutionForMouse = true;
+            CirclePointLast = e.GetPosition(CAN);
+            Mouse.Capture(UpCircle);
+        }
+
         public void CreateRectangle(Canvas canvas)
         {
 
-            
+
             BorderBorder.Width = defoultSize;
             BorderBorder.Height = defoultSize;
             BorderBorder.Child = canvas1;
@@ -84,13 +166,13 @@ namespace WPF1
         private void SHAP_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
         {
 
-            if(Solution == false) { return; }
+            if (Solution == false) { return; }
 
             Point MouseFirstPosition = e.GetPosition(CAN);
 
-           double left =  Canvas.GetLeft(BorderBorder);
+            double left = Canvas.GetLeft(BorderBorder);
             double top = Canvas.GetTop(BorderBorder);
-            
+
             Canvas.SetLeft(BorderBorder, left + MouseFirstPosition.X - MouesLastPosition.X);
             Canvas.SetTop(BorderBorder, top + MouseFirstPosition.Y - MouesLastPosition.Y);
 
@@ -102,10 +184,10 @@ namespace WPF1
 
         private void SHAP_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
-            
+
             Solution = false;
             SHAP.Fill = BaseColor;
-            BorderBorder.BorderBrush = null;
+            
             Mouse.Capture(null);
         }
 
@@ -113,16 +195,30 @@ namespace WPF1
         {
             MouesLastPosition = e.GetPosition(CAN);
             Solution = true;
-            BorderBorder.BorderBrush = Brushes.Yellow;
-            BorderBorder.BorderThickness = new Thickness(3);
+            
             canvas1.Background = Brushes.Blue;
             SHAP.Fill = FocusColor;
             Mouse.Capture(SHAP);
-            BorderBorder.BorderBrush = Brushes.DarkGreen;
+
+            if (OtDel != null)
+            {
+                OtDel(this);
+
+            }
+        }
+        public void Focus()
+        {
             UpCircle.Visibility = Visibility.Visible;
             DownCircle.Visibility = Visibility.Visible;
-
+            BorderBorder.BorderBrush = Brushes.Red;
 
         }
+        public void Blur()
+        {
+            UpCircle.Visibility = Visibility.Collapsed;
+            DownCircle.Visibility = Visibility.Collapsed;
+            BorderBorder.BorderBrush = null;
+        }
+
     }
 }
--- WPF1/MainWindow.xaml.cs
+++ WPF1/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,18 +21,57 @@ namespace WPF1
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Box selectedId = null;
+
+        private ObservableCollection<Box> box1 = new ObservableCollection<Box>();
+       
+
+       
+        
+
+       
         public MainWindow()
         {
             InitializeComponent();
            
         }
+        public
+         void Selected(Box select)
+        {
+            foreach (Box i in box1)
+            {
+                i.Blur();
+            }
+            select.Focus();
+
+            selectedId = select;
+
+
+        }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Box rectangle = new Box(MyCanvas, new Rectangle());
 
+            
 
+            
+            
+           rectangle.Link(Selected);
+            box1.Add(rectangle);
 
+        }
+        
+        public void Funki()
+        {
+            
+        }
 
+        private void MyCanvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.Source!= null) {
+                
+
+            }
         }
     }
 }
